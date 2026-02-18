37afc27bb95ff04724a7aec1a81a7276bafe00f2
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-02-18 22:25:55 +0900

    Title: L, R 그리고 B, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260218/L, R 그리고 B/l-r-and-b.py
@@ -0,0 +1,23 @@
+board = [list(input()) for _ in range(10)]
+
+# Please write your code here.
+b_pos = [-1, -1]
+l_pos = [-1, -1]
+
+for i, line in enumerate(board):
+    for j, elem in enumerate(line):
+        if elem == 'B':
+            b_pos = [i, j]
+        if elem == 'L':
+            l_pos = [i, j]
+
+if abs(b_pos[0] - l_pos[0]) - 1 >= 0 and abs(b_pos[1] - l_pos[1]) - 1 >= 0:
+    print(abs(b_pos[0] - l_pos[0]) - 1 + abs(b_pos[1] - l_pos[1]) - 1 + 1)
+elif abs(b_pos[0] - l_pos[0]) - 1 < 0:
+    print(abs(b_pos[1] - l_pos[1]) - 1)
+elif abs(b_pos[1] - l_pos[1]) - 1 < 0:
+    print(abs(b_pos[0] - l_pos[0]) - 1)
+else:
+    print(b_pos)
+    print(l_pos)
+
