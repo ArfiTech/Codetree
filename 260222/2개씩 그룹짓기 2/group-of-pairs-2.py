b6af9e681e2e18f15d6142a786aa30ec212468bd
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-02-22 22:12:26 +0900

    Title: 2개씩 그룹짓기 2, Time: 56ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 260222/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 260222/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -3,4 +3,7 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 arr.sort()
-print(arr[n+1] - arr[n-2])
+idx_1 = (n-1)//2
+idx_2 = ((2*n-1)+n)//2
+
+print(arr[idx_2] - arr[idx_1])
