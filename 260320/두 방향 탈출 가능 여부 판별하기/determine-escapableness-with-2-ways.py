7eec67f90a851066600b3c036a516eaf1b5a24f6
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-03-20 12:06:46 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 77ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260320/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,32 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+visited = [[False for _ in range(m)] for _ in range(n)]
+res = 0
+
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < m
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y] or edges[x][y] == 0:
+        return False
+    return True
+
+def dfs(x, y):
+    global res
+    dxs, dys = [1, 0], [0, 1]
+    # print("orig: ", x, y)
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if new_x == n-1 and new_y == m-1:
+            # print("--- ", new_x, new_y)
+            res = 1
+        if can_go(new_x, new_y):
+            # print(new_x, new_y)
+            dfs(new_x, new_y)
+
+dfs(0, 0)
+print(res)
