e29355b593bb0ef4d3d0625f1946f387e2cd1db0
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-03-20 20:09:17 +0900

    Title: 피보나치 수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260320/피보나치 수/fibonacci-number.py
+++ 260320/피보나치 수/fibonacci-number.py
@@ -2,23 +2,23 @@ n = int(input())
 
 # Please write your code here.
 # memoization
-# memo = [-1] * (n+1)
-# def fibb(n):
-#     if memo[n] != -1:
-#         return memo[n]
-#     if n <= 2:
-#         memo[n] = 1
-#     else:
-#         memo[n] = fibb(n-1) + fibb(n-2)
-#     return memo[n]
+memo = [-1] * (n+1)
+def fibb(n):
+    if memo[n] != -1:
+        return memo[n]
+    if n <= 2:
+        memo[n] = 1
+    else:
+        memo[n] = fibb(n-1) + fibb(n-2)
+    return memo[n]
 
-# print(fibb(n))
+print(fibb(n))
 
 # tabulation
-dp = [0] * (n+1)
-dp[1] = 1
-dp[2] = 1
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+# dp = [0] * (n+1)
+# dp[1] = 1
+# dp[2] = 1
+# for i in range(3, n+1):
+#     dp[i] = dp[i-1] + dp[i-2]
 
-print(dp[n])
+# print(dp[n])
