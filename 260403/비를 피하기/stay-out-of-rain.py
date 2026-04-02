33bc05fcf317ccf0625a40c02197593083923829
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-04-03 00:28:21 +0900

    Title: 비를 피하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260403/비를 피하기/stay-out-of-rain.py
+++ 260403/비를 피하기/stay-out-of-rain.py
@@ -7,14 +7,16 @@ import sys
 MAX_INT = sys.maxsize
 
 q = deque()
-pos_people = []
-pos_shelter = []
-for i in range(n):
-    for j in range(n):
-        if grid[i][j] == 2:
-            pos_people.append((i, j))
-        if grid[i][j] == 3:
-            pos_shelter.append((i, j))
+# pos_people = []
+# pos_shelter = []
+# for i in range(n):
+#     for j in range(n):
+#         if grid[i][j] == 2:
+#             pos_people.append((i, j))
+#         if grid[i][j] == 3:
+#             pos_shelter.append((i, j))
+visited = [[False for _ in range(n)] for _ in range(n)]
+steps = [[0 for _ in range(n)] for _ in range(n)]
 results = [[0 for _ in range(n)] for _ in range(n)]
 
 def in_range(x, y):
@@ -43,27 +45,42 @@ def bfs():
             if can_go(new_x, new_y):
                 push(new_x, new_y, steps[x][y] + 1)
 
-for x, y in pos_people:
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    steps = [[0 for _ in range(n)] for _ in range(n)]
-    push(x, y, 0)
-    bfs()
-    # print(steps)
-    is_shelter = False
-    min_shelter = MAX_INT
-    # res_x, res_y = -1, -1
-    for r_x, r_y in pos_shelter:
-        if steps[r_x][r_y] != 0 and min_shelter > steps[r_x][r_y]:
-            # res_x, res_y = r_x, r_y
-            min_shelter = steps[r_x][r_y]
-            is_shelter = True
-    if is_shelter:
-        results[x][y] = min_shelter
-    else:
-        results[x][y] = -1
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 3:
+            push(i, j, 0)
+bfs()
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 2:
+            results[i][j] = steps[i][j] if steps[i][j] != 0 else -1
 
 for i in range(n):
     for j in range(n):
         print(results[i][j], end=" ")
     print()
+
+# for x, y in pos_people:
+#     visited = [[False for _ in range(n)] for _ in range(n)]
+#     steps = [[0 for _ in range(n)] for _ in range(n)]
+#     push(x, y, 0)
+#     bfs()
+#     # print(steps)
+#     is_shelter = False
+#     min_shelter = MAX_INT
+#     # res_x, res_y = -1, -1
+#     for r_x, r_y in pos_shelter:
+#         if steps[r_x][r_y] != 0 and min_shelter > steps[r_x][r_y]:
+#             # res_x, res_y = r_x, r_y
+#             min_shelter = steps[r_x][r_y]
+#             is_shelter = True
+#     if is_shelter:
+#         results[x][y] = min_shelter
+#     else:
+#         results[x][y] = -1
+
+# for i in range(n):
+#     for j in range(n):
+#         print(results[i][j], end=" ")
+#     print()
     
