c1e715c5d5fdaca060ded9b304376968098bba73
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-04-11 22:35:30 +0900

    Title: 아름다운 수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260411/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int answer = 0;
+
+void DFS(int cur_len) {
+    if (cur_len >= n) {
+        if (cur_len == n)
+            answer++;
+        return;
+    }
+    for (int i = 1; i <= 4; i++) {
+        if (i == 1) {
+            DFS(cur_len + 1);
+        } else if (i == 2) {
+            DFS(cur_len + 2);
+        } else if (cur_len == 3) {
+            DFS(i + 3);
+        } else {
+            DFS(cur_len + 4);
+        }
+    }
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for (int i = 1; i <= 4; i++) {
+        DFS(i);
+    }
+    cout << answer << endl;
+    return 0;
+}
