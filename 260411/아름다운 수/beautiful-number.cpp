2a3a6d7ee9cfaed66ef629d55c35e1de441996c7
ArfiTech <60423885+ArfiTech@users.noreply.github.com>
2026-04-11 22:38:57 +0900

    Title: 아름다운 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260411/아름다운 수/beautiful-number.cpp
+++ 260411/아름다운 수/beautiful-number.cpp
@@ -15,8 +15,8 @@ void DFS(int cur_len) {
             DFS(cur_len + 1);
         } else if (i == 2) {
             DFS(cur_len + 2);
-        } else if (cur_len == 3) {
-            DFS(i + 3);
+        } else if (i == 3) {
+            DFS(cur_len + 3);
         } else {
             DFS(cur_len + 4);
         }
@@ -26,9 +26,10 @@ void DFS(int cur_len) {
 int main() {
     // Please write your code here.
     cin >> n;
-    for (int i = 1; i <= 4; i++) {
-        DFS(i);
-    }
+    // for (int i = 1; i <= 4; i++) {
+    //     DFS(i);
+    // }
+    DFS(0);
     cout << answer << endl;
     return 0;
 }
